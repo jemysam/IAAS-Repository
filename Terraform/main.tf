e66be3cc3cfcad38805c8b7571c7cd45ac5780db
jemysam <163811884+jemysam@users.noreply.github.com>
2024-03-18 11:51:54 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -4,7 +4,6 @@ terraform {
     resource_group_name  = "tamopstfstates"
     storage_account_name = "tamopstf111"
     container_name       = "tfstatedevops"
-    key                  = "tfstatedevops.tfstate"
   }
 }
  
