168bfac7d5d12fef87a0157957ca3b7453f5ccc2
jemysam <163811884+jemysam@users.noreply.github.com>
2024-03-18 12:31:40 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -9,7 +9,13 @@ terraform {
 }
  
 provider "azurerm" {
-  features {}
+    skip_provider_registration = true
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy    = true
+      recover_soft_deleted_key_vaults = true
+    }
+  }
 }
  
 data "azurerm_client_config" "current" {}
