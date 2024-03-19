b9b306c6145fdda0cf6d78039c19685de0d387c6
jemysam <163811884+jemysam@users.noreply.github.com>
2024-03-19 16:09:02 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -35,7 +35,7 @@ resource "azurerm_virtual_network" "vnet" {
   resource_group_name = azurerm_resource_group.tamops.name
 }
  
-# Create Subnet
+# Create Subnet1
 resource "azurerm_subnet" "subnet" {
   name                 = "subnet"
   resource_group_name  = azurerm_resource_group.tamops.name
